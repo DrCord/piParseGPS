1455fb6b1495a5a67d6427f23f654ce8fc0fec80
DrCord <cord42@gmail.com>
2016-05-20 15:45:43 -0700

    simple update to work with JSON structure of data, proof of concept for JSON via serial output working

--- piParseGPS.py
+++ piParseGPS.py
@@ -1,4 +1,13 @@
 import serial
-ser = serial.Serial('/dev/ttyACM0', 9600)
-while 1 :
-    ser.readline()
+import json
+ser = serial.Serial('/dev/ttyACM0', 115200)
+while 1:
+    parsed_data = json.loads(ser.readline())
+    print(parsed_data['sensor'])
+    print(parsed_data['time'])
+    print(parsed_data['longitude'])
+    print(parsed_data['latitude'])
+    print(parsed_data['altitude'])
+    print(parsed_data['speed'])
+    print(parsed_data['course'])
+    print('\n')
